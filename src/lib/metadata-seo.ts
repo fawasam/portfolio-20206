5e36868508695b2cf3528be4a7d795f4e8449254
fawasam <fawas@webcastle.in>
2026-03-30 00:46:49 +0530

    feat: update site icons to use favicon.ico and simplify metadata configuration

--- src/lib/metadata-seo.ts
+++ src/lib/metadata-seo.ts
@@ -417,17 +417,8 @@ export const generateMetadata = async (
     icons: {
       icon: [
         { url: "/favicon.ico" },
-        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       ],
-      apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
-      other: [
-        {
-          rel: "mask-icon",
-          url: "/safari-pinned-tab.svg",
-          color: defaultConfig.themeColor,
-        },
-      ],
+      apple: [{ url: "/favicon.ico" }],
     },
     manifest: "/site.webmanifest",
     verification: {
